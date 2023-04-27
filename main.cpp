71ecacfccc0975e4e3e84a0321ded41c60a91366
Kazutaka Nakashima <kazutaka.nakashima@n-taka.info>
2023-04-27 15:40:24 +0900

    [misc] rename, LICENSE text, etc...

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 // 
 // GPLv3
 //
-// This file is part of ZImportFolder.
+// This file is part of importFolder.
 // 
-// ZImportFolder is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// importFolder is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 // 
-// ZImportFolder is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// importFolder is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // 
-// You should have received a copy of the GNU General Public License along with ZImportFolder. If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License along with importFolder. If not, see <https://www.gnu.org/licenses/>.
 //
 
 #include <fstream>
